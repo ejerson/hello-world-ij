c7217942f7c77d7506541870bbefb1bc2cc1c719
0e0 <ejersonb@gmail.com>
2017-09-08 15:38:59 -0600

    Add salute method.

--- bootcamp/projects/hello-world-ij/src/bootcamp/edge/prework/HelloWorld.java
+++ bootcamp/projects/hello-world-ij/src/bootcamp/edge/prework/HelloWorld.java
@@ -3,6 +3,20 @@ package bootcamp.edge.prework;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+//        if (args.length > 0) {
+//            salute(args[0]);
+//        } else {
+//            salute("World");
+//        }
+
+        salute((args.length > 0) ? args[0] : "World");
+
+
     }
+
+    private static void salute(String target) {
+        System.out.print("Hello " + target + "!");
+    }
+
 }
